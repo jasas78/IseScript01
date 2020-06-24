11a953de875d50eeaed6ccca17f9cc10714cbf85
dyn <y@e>
2020-06-23 21:14:54 -0700

    _2020_0623__2114pm

--- Makefile.2091.vimAndGit.mk
+++ Makefile.2091.vimAndGit.mk
@@ -16,3 +16,30 @@ e : Makefile.env.mk
 	$(vim) $^
 
 
+# https://medium.com/@mlowicki/http-s-proxy-in-golang-in-less-than-100-lines-of-code-6a51c2f2c38c
+sslConfig:=/etc/ssl/openssl.cnf
+genssl:
+	[ -d ssl ] || mkdir ssl
+	[ -f ssl/myOpenssl.conf ] \
+		&& printf '\nAlread found : ssl/myOpenssl.conf , skip. \n\n' \
+		|| ( \
+		cat $(sslConfig)                              > ssl/myOpenssl.conf && \
+		printf '[SAN]\nsubjectAltName=DNS:localhost' >> ssl/myOpenssl.conf \
+		)
+	[ -f ssl/myServer.key ] \
+		&& printf '\nAlread found : ssl/myServer.key , skip. \n\n' \
+		|| \
+		openssl req \
+		-newkey rsa:2048 \
+		-x509 \
+		-nodes \
+		-keyout ssl/myServer.key \
+		-new \
+		-out ssl/myServer.pem \
+		-subj /CN=localhost \
+		-reqexts SAN \
+		-extensions SAN \
+		-config                 ssl/myOpenssl.conf    \
+		-sha256 \
+		-days 3650
+
